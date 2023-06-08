81cc72facb737fe7f303e1807cbd2896e7f572f4
geekyhimanshu2309 <himanshu112760@gmail.com>
2023-06-08 23:20:35 +0530

    Commit changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 const Dashboard = () => {
+  const fetchData = async() =>{
+    try {
+
+    } catch(error){
+      console.log(error)
+    }
+    const response = await fetch('https://localhost:5000/')
+    const data = await response.json()
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchData()
+  },[])
   return (
     <div>Dashboard Pages</div>
   )
 }
-
+ 
 export default Dashboard
